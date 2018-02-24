44f075cf68f85340dd09d3deb2065c4b346e65b2
Brendan Greene <brendanjgreene@gmail.com>
2018-02-24 13:50:46 +0000

    REPOSITORY

---
+++ src/test/java/com/qa/service/repository/AccountMapRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.qa.service.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountMapRepositoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
